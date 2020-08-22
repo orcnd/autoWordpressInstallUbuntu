1df71029e6d9a0bbb5724c4096fb7b1c30f1a0a1
orcun <orcuncandan89@gmail.com>
2020-08-22 03:58:42 +0300

    Update wpinstall.php

--- wpinstall.php
+++ wpinstall.php
@@ -1,20 +1,15 @@
 <?php 
-$wpdbpass=substr(md5(microtime()),12);
-$c = new PDO('mysql:host=localhost;', 'root', '');
+$wpdbpass=substr(md5(microtime()),0,8);
+$c=new PDO('mysql:host=localhost;', 'root', '');
 $c->query("create database wpdb");
 
 $c->query("create user wpuser@localhost identified by '" . $wpdbpass . "'");
 $c->query("grant all privileges on wpdb.* to wpuser@localhost");
 $c->query("FLUSH PRIVILEGES");
 
-
-
-shell_exec("wget https://tr.wordpress.org/latest-tr_TR.tar.gz");
 shell_exec("tar -xzf latest-tr_TR.tar.gz");
 shell_exec("rm -rf /var/www/html/*");
 shell_exec("mv wordpress/* /var/www/html");
-
-
 
 $cf=file_get_contents('/var/www/html/wp-config-sample.php');
 $cf=str_replace("veritabaniismi","wpdb",$cf);
@@ -26,3 +21,4 @@ file_put_contents('/var/www/html/wp-config.php',$cf);
 
 shell_exec("chmod 755 /var/www/html");
 shell_exec("systemctl restart apache2");
+shell_exec("systemctl restart mysql");
